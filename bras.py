e336064c8eaf5878eec3428ca17156b1bad86644
Dongliang Yu <yudl.nju@gmail.com>
2014-02-24 11:03:38 +0800

    add connect

--- bras.py
+++ bras.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-path = "http://bras.nju.edu.cn:8080/selfservice"
+portal_path = "http://p.nju.edu.cn/portal/portal_io.do"
+bras_path = "http://bras.nju.edu.cn:8080/selfservice"
 
 def read_config(file_path):
     with open(file_path, 'r') as f:
@@ -10,9 +11,21 @@ def read_config(file_path):
         password = content[1]
     return username, password
 
-def login(username, password):
+def connect(username, password):
     hd = {"Content-type": "application/json","Accept": "application/json"}
-    url = path + '/login'
+    url = portal_path
+    data = {'action' : 'login', 'username' : username,'password' : password}
+    response = requests.post(url, data=data, headers=hd)
+    result = response.json()
+    reply_code = result['reply_code']
+    if reply_code == 101:
+        print 'connect successfully!'
+    else:
+        print 'connect failed:', result['reply_message']
+
+def bras_service_login(username, password):
+    hd = {"Content-type": "application/json","Accept": "application/json"}
+    url = bras_path + '/login'
     user_info = {'username':username,'password':password}
     response = requests.post(url, params=user_info, headers=hd)
     result = response.json()
@@ -22,13 +35,11 @@ def login(username, password):
     else:
         print 'login failed: ', result['reply_msg']
     jsessionid = response.cookies._cookies['bras.nju.edu.cn']['/']['JSESSIONID'].value
-    #print 'result::', result
-    #print 'cookies::', response.cookies._cookies['bras.nju.edu.cn']['/']['JSESSIONID'].value
     return {'JSESSIONID' : jsessionid}
 
 def disconnect(uid, cookie):
     hd = {"Content-type": "application/x-www-form-urlencoded","Accept": "application/json"}
-    url = path + '/disconnect'
+    url = bras_path + '/disconnect'
     params = {'id':uid}
     response = requests.post(url, data=params, headers=hd, cookies=cookie)
     result = response.json()
@@ -37,11 +48,10 @@ def disconnect(uid, cookie):
         print 'disconnect successfully!'
     else:
         print 'disconnect failed: ', result['reply_msg']
-    #print response.__dict__
 
-def online(cookie):
+def get_online_info(cookie):
     hd = {"Content-type": "application/json","Accept": "application/json"}
-    url = path + '/online'
+    url = bras_path + '/online'
     response = requests.post(url, headers=hd, cookies=cookie)
     result = response.json()
     reply_code = result['reply_code']
@@ -49,10 +59,10 @@ def online(cookie):
         print 'online successfully!'
     else:
         print 'online failed: ', result['reply_msg']
-    #print result['results']['rows']
     return result['results']['rows'][0]['id']
 
 user_info = read_config('bras_config')
-cookie = login(user_info[0], user_info[1])
-uid = online(cookie)
+#connect(user_info[0], user_info[1])
+cookie = bras_service_login(user_info[0], user_info[1])
+uid = get_online_info(cookie)
 disconnect(uid, cookie)
